1aea6e74e8f18d31572f5b911d931f03eca49e16
bhaskar-ascendion <bhaskar.sharma@ascendion.com>
2024-06-04 11:51:35 +0000

    first copilot file

---
+++ skills.js
@@ -0,0 +1,8 @@
+function calculateNumbers(var1, var2)
+{console.log(var1+var2);}
+
+function deductNumbers(var1, var2)
+{console.log(var1-var2);}
+
+function multiplyNumbers(var1, var2)
+{console.log(var1*var2);}
